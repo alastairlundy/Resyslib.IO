114b02a928777cf7344d48e155562a0d2c490145
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-19 16:34:16 +0000

    add missing copyright notice

--- AlastairLundy.Extensions.IO/AlastairLundy.Extensions.IO/Directories/RecursiveDirectoryManager.cs
+++ AlastairLundy.Extensions.IO/AlastairLundy.Extensions.IO/Directories/RecursiveDirectoryManager.cs
@@ -1,6 +1,16 @@
-﻿using System.Collections.Generic;
+﻿/*
+    IOExtensions 
+    Copyright (c) 2024-2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+
 using AlastairLundy.Extensions.IO.Directories.Abstractions;
 using AlastairLundy.Extensions.IO.Internal.Localizations;
 // ReSharper disable RedundantBoolCompare
