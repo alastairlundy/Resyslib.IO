b87b09577cc1a8880ba7b704f1eb6bf94b069558
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-20 17:42:12 +0000

    re-add IFilePathResolver

---
+++ AlastairLundy.Extensions.IO/AlastairLundy.Extensions.IO/Files/Abstractions/IFilePathResolver.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace AlastairLundy.Extensions.IO.Files;
+
+public interface IFilePathResolver
+{
+    /// <summary>
+    /// Resolves the file path to a specific file if the file path is part of a PATH environment variable.
+    /// </summary>
+    /// <param name="inputFilePath">The input file path to resolve.</param>
+    /// <param name="outputFilePath">The resolved file path if the file path is in the PATH environment variable; the original input file path otherwise.</param>
+    /// <exception cref="FileNotFoundException">Thrown if the input file path is null or empty.</exception>
+    void ResolveFilePath(string inputFilePath, out string outputFilePath);
+}
