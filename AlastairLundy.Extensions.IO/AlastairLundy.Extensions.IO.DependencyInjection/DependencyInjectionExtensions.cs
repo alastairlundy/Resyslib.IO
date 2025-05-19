860025571a4cea7736f085670fd795df322bb848
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-19 18:32:24 +0100

    update DI extension package to 3.0.0

--- AlastairLundy.Extensions.IO/AlastairLundy.Extensions.IO.DependencyInjection/DependencyInjectionExtensions.cs
+++ AlastairLundy.Extensions.IO/AlastairLundy.Extensions.IO.DependencyInjection/DependencyInjectionExtensions.cs
@@ -1,6 +1,6 @@
 ﻿/*
     AlastairLundy.Extensions.IO 
-    Copyright (c) 2025 Alastair Lundy
+    Copyright (c) 2024-2025 Alastair Lundy
 
     This Source Code Form is subject to the terms of the Mozilla Public
     License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,57 +8,60 @@
  */
 
 using AlastairLundy.Extensions.IO.Directories;
-using AlastairLundy.Extensions.IO.Directories.Abstractions;
+using AlastairLundy.Extensions.IO.Abstractions.Directories;
+
 using AlastairLundy.Extensions.IO.Files;
-using AlastairLundy.Extensions.IO.Files.Abstractions;
 using AlastairLundy.Extensions.IO.Files.Concatenation;
-using AlastairLundy.Extensions.IO.Files.Concatenation.Abstractions;
+
+using AlastairLundy.Extensions.IO.Abstractions.Files;
+using AlastairLundy.Extensions.IO.Abstractions.Files.Concatenation;
 
 using Microsoft.Extensions.DependencyInjection;
 // ReSharper disable InconsistentNaming
 
-namespace AlastairLundy.Extensions.IO.DependencyInjection;
-
-public static class DependencyInjectionExtensions
+namespace AlastairLundy.Extensions.IO.DependencyInjection
 {
-
-    /// <summary>
-    /// Sets up Dependency Injection for IO Extensions' interface-able types.
-    /// </summary>
-    /// <param name="services">The service collection to add to.</param>
-    /// <param name="lifetime">The service lifetime to use if specified; Singleton otherwise.</param>
-    /// <returns>The updated service collection with the added IO Extensions dependency injection setup.</returns>
-    public static IServiceCollection AddExtensionsDotIO(this IServiceCollection services,
-        ServiceLifetime lifetime = ServiceLifetime.Singleton)
+    public static class DependencyInjectionExtensions
     {
-        switch (lifetime)
+
+        /// <summary>
+        /// Sets up Dependency Injection for IO Extensions' interface-able types.
+        /// </summary>
+        /// <param name="services">The service collection to add to.</param>
+        /// <param name="lifetime">The service lifetime to use if specified; Singleton otherwise.</param>
+        /// <returns>The updated service collection with the added IO Extensions dependency injection setup.</returns>
+        public static IServiceCollection AddExtensionsDotIO(this IServiceCollection services,
+            ServiceLifetime lifetime = ServiceLifetime.Singleton)
         {
-            case ServiceLifetime.Singleton:
-                services.AddSingleton<IFileFinder, FileFinder>();
-                services.AddSingleton<IFileAppender, FileAppender>();
-                services.AddSingleton<IFileConcatenator, FileConcatenator>();
-                services.AddSingleton<IRecursiveDirectoryManager, RecursiveDirectoryManager>();
-                services.AddSingleton<IParentDirectoryManager, ParentDirectoryManager>();
-                services.AddSingleton<IFilePathResolver, FilePathResolver>();
-                break;
-            case ServiceLifetime.Scoped:
-                services.AddScoped<IFileFinder, FileFinder>();
-                services.AddScoped<IFileAppender, FileAppender>();
-                services.AddScoped<IFileConcatenator, FileConcatenator>();
-                services.AddScoped<IRecursiveDirectoryManager, RecursiveDirectoryManager>();
-                services.AddScoped<IParentDirectoryManager, ParentDirectoryManager>();
-                services.AddScoped<IFilePathResolver, FilePathResolver>();
-                break;
-            case ServiceLifetime.Transient:
-                services.AddTransient<IFileFinder, FileFinder>();
-                services.AddTransient<IFileAppender, FileAppender>();
-                services.AddTransient<IFileConcatenator, FileConcatenator>();
-                services.AddTransient<IRecursiveDirectoryManager, RecursiveDirectoryManager>();
-                services.AddTransient<IParentDirectoryManager, ParentDirectoryManager>();
-                services.AddTransient<IFilePathResolver, FilePathResolver>();
-                break;
-        }
+            switch (lifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    services.AddSingleton<IFileFinder, FileFinder>();
+                    services.AddSingleton<IFileAppender, FileAppender>();
+                    services.AddSingleton<IFileConcatenator, FileConcatenator>();
+                    services.AddSingleton<IRecursiveDirectoryManager, RecursiveDirectoryManager>();
+                    services.AddSingleton<IParentDirectoryManager, ParentDirectoryManager>();
+                    services.AddSingleton<IFilePathResolver, FilePathResolver>();
+                    break;
+                case ServiceLifetime.Scoped:
+                    services.AddScoped<IFileFinder, FileFinder>();
+                    services.AddScoped<IFileAppender, FileAppender>();
+                    services.AddScoped<IFileConcatenator, FileConcatenator>();
+                    services.AddScoped<IRecursiveDirectoryManager, RecursiveDirectoryManager>();
+                    services.AddScoped<IParentDirectoryManager, ParentDirectoryManager>();
+                    services.AddScoped<IFilePathResolver, FilePathResolver>();
+                    break;
+                case ServiceLifetime.Transient:
+                    services.AddTransient<IFileFinder, FileFinder>();
+                    services.AddTransient<IFileAppender, FileAppender>();
+                    services.AddTransient<IFileConcatenator, FileConcatenator>();
+                    services.AddTransient<IRecursiveDirectoryManager, RecursiveDirectoryManager>();
+                    services.AddTransient<IParentDirectoryManager, ParentDirectoryManager>();
+                    services.AddTransient<IFilePathResolver, FilePathResolver>();
+                    break;
+            }
         
-        return services;
+            return services;
+        }
     }
 }
