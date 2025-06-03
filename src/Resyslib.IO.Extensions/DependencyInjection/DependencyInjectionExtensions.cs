2ec1d4d197337c18ed3a7b28ad5d9f8d863453d2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-03 10:42:38 +0100

    update to 3.1.0

--- src/Resyslib.IO.Extensions/DependencyInjection/DependencyInjectionExtensions.cs
+++ src/Resyslib.IO.Extensions/DependencyInjection/DependencyInjectionExtensions.cs
@@ -26,39 +26,39 @@ namespace AlastairLundy.Resyslib.IO.Extensions.DependencyInjection
     {
 
         /// <summary>
-        /// Sets up Dependency Injection for IO Extensions' interface-able types.
+        /// Sets up Dependency Injection for Resyslib.IO's interface-able types.
         /// </summary>
         /// <param name="services">The service collection to add to.</param>
         /// <param name="lifetime">The service lifetime to use if specified; Singleton otherwise.</param>
-        /// <returns>The updated service collection with the added IO Extensions dependency injection setup.</returns>
-        public static IServiceCollection AddExtensionsDotIO(this IServiceCollection services,
+        /// <returns>The updated service collection with the added Resyslib.IO's dependency injection setup.</returns>
+        public static IServiceCollection AddResyslibIO(this IServiceCollection services,
             ServiceLifetime lifetime = ServiceLifetime.Singleton)
         {
             switch (lifetime)
             {
                 case ServiceLifetime.Singleton:
                     services.AddSingleton<IFileFinder, FileFinder>();
+                    services.AddSingleton<IFilePathResolver, FilePathResolver>();
                     services.AddSingleton<IFileAppender, FileAppender>();
                     services.AddSingleton<IFileConcatenator, FileConcatenator>();
                     services.AddSingleton<IRecursiveDirectoryManager, RecursiveDirectoryManager>();
                     services.AddSingleton<IParentDirectoryManager, ParentDirectoryManager>();
-                    services.AddSingleton<IFilePathResolver, FilePathResolver>();
                     break;
                 case ServiceLifetime.Scoped:
                     services.AddScoped<IFileFinder, FileFinder>();
+                    services.AddScoped<IFilePathResolver, FilePathResolver>();
                     services.AddScoped<IFileAppender, FileAppender>();
                     services.AddScoped<IFileConcatenator, FileConcatenator>();
                     services.AddScoped<IRecursiveDirectoryManager, RecursiveDirectoryManager>();
                     services.AddScoped<IParentDirectoryManager, ParentDirectoryManager>();
-                    services.AddScoped<IFilePathResolver, FilePathResolver>();
                     break;
                 case ServiceLifetime.Transient:
                     services.AddTransient<IFileFinder, FileFinder>();
+                    services.AddTransient<IFilePathResolver, FilePathResolver>();
                     services.AddTransient<IFileAppender, FileAppender>();
                     services.AddTransient<IFileConcatenator, FileConcatenator>();
                     services.AddTransient<IRecursiveDirectoryManager, RecursiveDirectoryManager>();
                     services.AddTransient<IParentDirectoryManager, ParentDirectoryManager>();
-                    services.AddTransient<IFilePathResolver, FilePathResolver>();
                     break;
             }
         
